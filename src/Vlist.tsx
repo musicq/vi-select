24aa75bf9358aac76183e156d238cc5df3d195c0
musicq <liang.kui@hotmail.com>
2018-12-27 13:40:48 +0800

    feat: input will change when the value and placeholder change

---
+++ src/Vlist.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class Vlist extends React.Component {
+  render() {
+    return (
+      <div style={{ boxShadow: '0 0 10px black', background: 'white' }}>111</div>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,82 @@
-/**
- * @class ExampleComponent
- */
+import { Dropdown, Input } from 'antd';
+import * as React from 'react';
+import { ChangeEvent } from 'react';
+import { fromEvent } from 'rxjs';
+import { Vlist } from './Vlist';
 
-import * as React from 'react'
+interface IVSelectProps {
+  placeholder?: string;
+  value?: string;
+  onChange?: Function;
+}
 
-import styles from './styles.css'
+interface IVSelectState {
+  visible: boolean;
+  placeholder: string | undefined;
+  value: string | undefined;
+  originValue: string | undefined;
+  originPlaceholder: string | undefined;
+}
 
-export type Props = { text: string }
+export class VSelect extends React.Component<IVSelectProps, IVSelectState> {
+  state = {
+    visible: false,
+    placeholder: undefined,
+    value: undefined,
 
-export default class ExampleComponent extends React.Component<Props> {
+    originValue: undefined,
+    originPlaceholder: undefined
+  };
+
+  constructor(props: any) {
+    super(props);
+
+    this.onShow = this.onShow.bind(this);
+    this.onInput = this.onInput.bind(this);
+  }
+
+  static getDerivedStateFromProps(props: IVSelectProps, state: IVSelectState) {
+    const ns = {} as IVSelectState;
+
+    if (props.placeholder !== state.originPlaceholder) {
+      ns.placeholder = props.placeholder;
+      ns.originPlaceholder = props.placeholder;
+    }
+
+    if (props.value !== state.originValue) {
+      ns.value = props.value;
+      ns.originValue = props.value;
+    }
+
+    return Object.keys(ns).length ? ns : null;
+  }
+
+  componentDidMount(): void {
+    fromEvent(document.body, 'click').subscribe(() => {
+      if (this.state.visible) {
+        this.setState({ visible: false });
+      }
+    });
+  }
+
+  onShow() {
+    this.setState({ visible: true });
+  }
+
+  onInput(e: ChangeEvent) {
+    this.setState({ value: (e.target as HTMLInputElement).value });
+  }
+
   render() {
-    const {
-      text
-    } = this.props
-
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+      <Dropdown overlay={<Vlist/>} visible={this.state.visible}>
+        <Input
+          placeholder={this.state.placeholder}
+          value={this.state.value}
+          onChange={this.onInput}
+          onClick={this.onShow}/>
+      </Dropdown>
+    );
   }
 }
+
