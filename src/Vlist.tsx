7e48a20f8b9151d19ec04a3e2c579db3a241f12b
musicq <liang.kui@hotmail.com>
2018-12-27 17:30:14 +0800

    feat: add ability of select

--- src/Vlist.tsx
+++ src/Vlist.tsx
@@ -1,9 +1,39 @@
 import * as React from 'react';
+import { Observable, of } from 'rxjs';
+import { VirtualList } from 'vist';
+import style from './styles.css';
 
-export class Vlist extends React.Component {
+interface IVlistProps<T> {
+  data$: Observable<T[]>;
+  onChange: (v: T) => void;
+}
+
+export class Vlist<T> extends React.Component<IVlistProps<T>> {
+  constructor(props: any) {
+    super(props);
+
+    this.onSelect = this.onSelect.bind(this);
+  }
+
+  onSelect(e: T) {
+    this.props.onChange(e);
+  }
+
   render() {
     return (
-      <div style={{ boxShadow: '0 0 10px black', background: 'white' }}>111</div>
+      <div className="ant-dropdown-menu">
+        <VirtualList
+          data$={this.props.data$}
+          options$={of({ height: 90 })}
+          style={{ height: 400 }}
+        >
+          {(item: T) => (
+            <div className={style.VlistItem} onClick={() => this.onSelect(item)}>
+              {(this.props.children as any)(item)}
+            </div>
+          )}
+        </VirtualList>
+      </div>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,46 @@
 import { Dropdown, Input } from 'antd';
 import * as React from 'react';
 import { ChangeEvent } from 'react';
-import { fromEvent } from 'rxjs';
+import { BehaviorSubject, combineLatest, fromEvent, Observable, Subscription } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 import { Vlist } from './Vlist';
 
-interface IVSelectProps {
+interface IVSelectProps<T> {
   placeholder?: string;
   value?: string;
   onChange?: Function;
+  dataSource: T[];
+  searchField: string;
 }
 
-interface IVSelectState {
+interface IVSelectState<T> {
   visible: boolean;
   placeholder: string | undefined;
   value: any | undefined;
   originValue: any | undefined;
   originPlaceholder: any | undefined;
   lastValue: any | undefined;
+  dataSourceSnapshot: T[];
+  dataSource$: BehaviorSubject<T[]>;
+  value$: BehaviorSubject<string | undefined>;
 }
 
-export class VSelect extends React.Component<IVSelectProps, IVSelectState> {
+export class VSelect<T> extends React.Component<IVSelectProps<T>, IVSelectState<T>> {
   state = {
     visible: false,
     placeholder: undefined,
     value: undefined,
+    dataSourceSnapshot: [] as T[],
 
     originValue: undefined,
     originPlaceholder: undefined,
-    lastValue: undefined
+    lastValue: undefined,
+    dataSource$: new BehaviorSubject<T[]>([]),
+    value$: new BehaviorSubject<string>('')
   };
+
+  private _sub: Subscription[] = [];
+  private dataSource$: Observable<T[]>;
 
   constructor(props: any) {
     super(props);
@@ -36,11 +48,12 @@ export class VSelect extends React.Component<IVSelectProps, IVSelectState> {
     this.onShow = this.onShow.bind(this);
     this.onInput = this.onInput.bind(this);
     this.onClose = this.onClose.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   // 为了对比是否外界传入的参数变化，如果变化则改变内在的状态，外部优先
-  static getDerivedStateFromProps(props: IVSelectProps, state: IVSelectState) {
-    const ns = {} as IVSelectState;
+  static getDerivedStateFromProps<T>(props: IVSelectProps<T>, state: IVSelectState<T>) {
+    const ns = {} as IVSelectState<T>;
 
     if (props.placeholder !== state.originPlaceholder) {
       ns.placeholder = props.placeholder;
@@ -50,20 +63,41 @@ export class VSelect extends React.Component<IVSelectProps, IVSelectState> {
     if (props.value !== state.originValue) {
       ns.value = props.value;
       ns.originValue = props.value;
+      state.value$.next(props.value);
+    }
+
+    if (props.dataSource !== state.dataSourceSnapshot) {
+      ns.dataSourceSnapshot = props.dataSource;
+      state.dataSource$.next(props.dataSource);
     }
 
     return Object.keys(ns).length ? ns : null;
   }
 
   componentDidMount(): void {
-    fromEvent(document.body, 'click').subscribe(e => {
-      const clsList = Array.from((e.target as HTMLElement).classList);
+    this._sub.push(
+      fromEvent(document.body, 'click').subscribe(e => {
+        const clsList = Array.from((e.target as HTMLElement).classList);
 
-      // click elements except dropdown panel
-      if (!clsList.includes('ant-dropdown') && this.state.visible) {
-        this.setState({ visible: false });
-      }
-    });
+        // click elements except dropdown panel
+        if (!clsList.includes('ant-dropdown') && this.state.visible) {
+          this.setState({ visible: false });
+        }
+      })
+    );
+
+    this.dataSource$ = combineLatest(this.state.dataSource$, this.state.value$).pipe(
+      distinctUntilChanged(),
+      map(([dataSource, value]) => dataSource.filter(
+        source => value
+          ? source[this.props.searchField].toString().toLowerCase().includes(value.toString().toLowerCase())
+          : dataSource
+      ))
+    );
+  }
+
+  componentWillUnmount(): void {
+    this._sub.forEach(s => s.unsubscribe());
   }
 
   onShow() {
@@ -71,6 +105,7 @@ export class VSelect extends React.Component<IVSelectProps, IVSelectState> {
     const placeholder = lastValue === undefined ? this.state.placeholder : lastValue;
     const value = undefined;
 
+    this.state.value$.next('');
     this.setState({ visible: true, placeholder, value, lastValue });
   }
 
@@ -90,17 +125,32 @@ export class VSelect extends React.Component<IVSelectProps, IVSelectState> {
   }
 
   onInput(e: ChangeEvent) {
+    this.state.value$.next((e.target as HTMLInputElement).value);
     this.setState({ value: (e.target as HTMLInputElement).value });
+  }
+
+  onChange(v: T) {
+    if (this.props.onChange) {
+      this.props.onChange(v);
+    }
+
+    this.setState({ value: v[this.props.searchField] });
   }
 
   render() {
     return (
-      <Dropdown overlay={<Vlist/>} trigger={['click']} visible={this.state.visible} onVisibleChange={this.onClose}>
+      <Dropdown
+        overlay={<Vlist data$={this.dataSource$} children={this.props.children} onChange={this.onChange}/>}
+        trigger={['click']}
+        visible={this.state.visible}
+        onVisibleChange={this.onClose}
+      >
         <Input
           placeholder={this.state.placeholder}
           value={this.state.value}
           onChange={this.onInput}
-          onClick={this.onShow}/>
+          onClick={this.onShow}
+        />
       </Dropdown>
     );
   }
